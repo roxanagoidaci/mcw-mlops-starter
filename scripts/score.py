1a3af7275fc0c0f5fc7d4b74ad0be75650f43575
Roxana Goidaci <roxana.goidaci@genisoft.eu>
2020-03-18 05:01:32 +0200

    score accepts text as input

--- scripts/score.py
+++ scripts/score.py
@@ -12,14 +12,28 @@ from azureml.monitoring import ModelDataCollector
 import onnxruntime
 
 
-
-
-
 def init():
     global model
     global inputs_dc, prediction_dc
-    
+    global tokenizer
+    global maxlen
     try:
+
+        data_url = ('https://quickstartsws9073123377.blob.core.windows.net/'
+                    'azureml-blobstore-0d1c4218-a5f9-418b-bf55-902b65277b85/'
+                    'quickstarts/connected-car-data/connected-car_components.csv')
+
+        # Load the car components labeled data
+        car_components_df = pd.read_csv(data_url)
+        components = car_components_df["text"].tolist()
+        labels = car_components_df["label"].tolist()
+
+        maxlen = 100                                               
+        max_words = 10000      
+
+        tokenizer = Tokenizer(num_words=max_words)
+        tokenizer.fit_on_texts(components)
+
         model_name = 'MODEL-NAME' # Placeholder model name
         print('Looking for model path for model: ', model_name)
         model_path = Model.get_model_path(model_name = model_name)
@@ -41,7 +55,11 @@ def run(raw_data):
     try:
         print("Received input: ", raw_data)
         
-        inputs = np.array(json.loads(raw_data)).astype(np.float32)
+
+        inputs = np.array(json.loads(raw_data))
+
+        sequences = tokenizer.texts_to_sequences(inputs)
+        data = pad_sequences(sequences, maxlen=maxlen)
        
         results = model.run(None, {model.get_inputs()[0].name:inputs})[0]
         results = results[0][0].item()
